1b0eeaf3ff16f218d2b252716d217fc503e09079
Garret Gallant <gallanttechnological@gmail.com>
2022-06-15 16:28:33 -0600

    created header, and nav table. Included styling and subtle animations on hover

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+/* UNUSED h1
 
-document.querySelector("body").appendChild(h2);
+const h1 = document.createElement("h1");
+h1.textContent = "missingno";
+
+document.querySelector("body").appendChild(h1);
+
+*/
